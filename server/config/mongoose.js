6776d36fe615a633d5c23369b981f1461453ce6a
Roger Tafoya <sillabus@aol.com>
2016-04-25 22:42:30 +0000

    First Commit

---
+++ server/config/mongoose.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var fs = require('fs');
+mongoose.connect("mongodb://localhost/blackBelt");
+var modelsPath = __dirname + '/../models';
+fs.readdirSync(modelsPath).forEach(function(file){
+	if(file.indexOf('js')>0){
+		require(modelsPath + '/' + file);
+	}
+});
